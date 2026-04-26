c82f4fcb3d6e31533ba7a9047133880b11ba5fa8
optimum42 <holgermassek@gmail.com>
2026-04-26 12:19:22 +0200

    MVP version

--- game_logic.py
+++ game_logic.py
@@ -6,7 +6,7 @@ WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
 
 def get_random_word():
-    """Selects a random word from the list."""
+    """ Selects a random word from the list of words """
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
@@ -14,22 +14,40 @@ def display_game_state(mistakes, secret_word, guessed_letters):
     """
     Displays the current meltdown stage and the secret word state
     with the guessed letters uncovered
+    :return: True if the word is completely guessed, False otherwise
     """
     print(STAGES[mistakes])
     masked = [c if c in guessed_letters else '_' for c in secret_word]
     print("Word: " + " ".join(masked))
+    return '_' not in masked
 
 
 def play_game():
+    """
+    paying loop
+    the loop end if either the secret word is guessed (win)
+    or the snowman is melding down (lose)
+    """
     secret_word = get_random_word()
-    guessed_letters = ['a', 'e', 'i', 's', 'o', 'u']
+    guessed_letters = []
     mistakes = 0
 
     print("Welcome to Snowman Meltdown!")
-    display_game_state(mistakes, secret_word, guessed_letters)
 
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+    # game loop
+    while mistakes < len(STAGES):
+        if display_game_state(mistakes, secret_word, guessed_letters):
+            break
+        guess = input("\nGuess a letter: ").lower()
+        if guess in guessed_letters:
+            print("You already guessed that letter!")
+        else:
+            guessed_letters.append(guess)
+        if guess not in secret_word:
+            mistakes += 1
+
+    if mistakes == len(STAGES):
+        print("Sorry, snowman melted down...")
+    else:
+        print("Congrats! You guessed the word!")
 
